ec44cd681c71628d218f598d3248739fdf1571d5
sueraychang <sueraychang@gmail.com>
2016-11-09 18:57:21 +0800

    Add some logs for example.

--- app/src/main/java/com/src/filelogger/MainActivity.java
+++ app/src/main/java/com/src/filelogger/MainActivity.java
@@ -5,14 +5,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "[FileLogger]" + MainActivity.class.getSimpleName();
+
+    private Logger mLogger = LoggerFactory.getLogger(MainActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+        mLogger.debug("onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -22,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "On FloatingActionButton click");
+                mLogger.debug("On FloatingActionButton click");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -44,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.d(TAG, "Select option item : setting");
+            mLogger.debug("Select option item : setting");
             return true;
         }
 
